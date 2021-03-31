6bdae9256c713f9bdb6268a1494bdaa67953786f
soulgo <1406221797@qq.com>
2021-03-31 17:13:03 +0800

    日志测试

---
+++ src/code.lxj.com/mylogger/mylogger.go
@@ -0,0 +1,71 @@
+package mylogger
+
+import (
+	"errors"
+	"fmt"
+	"path"
+	"runtime"
+	"strings"
+)
+
+//在终端写日志相关内容
+type LogLevel uint16
+
+//常量
+const (
+	Unknown LogLevel = iota
+	DEBUG
+	INFO
+	WARNING
+	ERROR
+	FATAL
+)
+
+//日志开关
+func parseLogLevel(s string) (LogLevel, error) {
+	s = strings.ToLower(s)
+	switch s {
+	case "debug":
+		return DEBUG, nil
+	case "info":
+		return INFO, nil
+	case "warning":
+		return WARNING, nil
+	case "error":
+		return ERROR, nil
+	case "fatal":
+		return FATAL, nil
+	default:
+		err := errors.New("无效得日志级别")
+		return Unknown, err
+	}
+}
+
+func getLogString(lv LogLevel) string {
+	switch lv {
+	case DEBUG:
+		return "DEBUG"
+	case INFO:
+		return "INFO"
+	case WARNING:
+		return "WARNING"
+	case FATAL:
+		return "FATAL"
+	case ERROR:
+		return "ERROR"
+	}
+	return "DEBUG"
+}
+
+//获取文件名、行数
+func getInfo(skip int) (funcName, fileName string, lineNo int) {
+	pc, file, lineNo, ok := runtime.Caller(skip)
+	if !ok {
+		fmt.Printf("runtime.Caller failed/n")
+		return
+	}
+	funcName = runtime.FuncForPC(pc).Name()
+	fileName = path.Base(file)
+	funcName = strings.Split(funcName, ".")[1]
+	return
+}
--- src/code.lxj.com/mylogger_test/main.go
+++ src/code.lxj.com/mylogger_test/main.go
@@ -6,12 +6,15 @@ import (
 )
 
 func main() {
-	log := mylogger.NewLog("fatal")
+	log := mylogger.NewFileLogger("info", "./", "lxj.log", 10*1024)
 	for {
 		log.Debug("这是一条debug日志")
 		log.Info("这是一条Info日志")
 		log.Warning("这是一条Warning日志")
-		log.Error("这是一条Error日志")
+		a := 100
+		b := "理想"
+		c := "北京大学"
+		log.Error("这是一条Error日志，id:%d,name:%s,address:%s", a, b, c)
 		log.Fatal("这是一条Fatal日志")
 		time.Sleep(time.Second * 2)
 	}
